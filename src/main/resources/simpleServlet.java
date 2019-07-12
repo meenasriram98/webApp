daabf35f99fef9ac37a3d748f24bd83bc102d548
meenasriram98 <meena.sriram@imaginea.com>
2019-07-12 14:21:10 +0530

    first commit

---
+++ src/main/resources/simpleServlet.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.annotation.WebServlet;
+
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+
+ * Servlet implementation class SimpleServlet
+
+ */
+
+@WebServlet("/hello")
+
+public class SimpleServlet extends HttpServlet {
+
+   private static final long serialVersionUID = 1L;
+
+   public SimpleServlet() {
+		// TODO Auto-generated constructor stub
+		   System.out.println("Servlet object created!!");
+	}
+   
+   @Override
+   public void doGet(HttpServletRequest req, HttpServletResponse resp) 
+
+         throws ServletException, IOException {
+
+      resp.setContentType("text/plain");
+
+      resp.getWriter().write("Hello World! Maven Web Project Example.");
+
+   }
+   
+
+}
